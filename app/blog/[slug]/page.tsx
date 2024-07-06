1560355f29b5c6c74d8a1f4fe3a0d54e62e40adc
Akshay <akshaythedesigner9@gmail.com>
2024-07-07 02:20:59 +0530

    added revalidate tags

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -3,6 +3,8 @@ import { client, urlFor } from "@/app/lib/sanity";
 import { PortableText } from "@portabletext/react";
 import Image from "next/image";
 
+export const revalidate = 30; //Revalidate after 30s
+
 async function getData(slug: string){
     const query = `
     *[_type == "blog" && slug.current == '${slug}'] {
